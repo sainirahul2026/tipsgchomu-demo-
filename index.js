e125f55b60dc301371963702ddd3ba7f02267f51
sainirahul2026 <rahulsaini20040701@gmail.com>
2026-05-06 19:22:57 +0530

    add new js file  and create new code

---
+++ index.js
@@ -0,0 +1,46 @@
+console.log("hello word");
+
+// alert("hello tipsg student")
+
+
+console.log("a");
+console.log("b");
+console.log("c");
+console.log("d");
+console.log("e");
+
+
+function add(number1, number2) {
+    console.log(number1 + number2);
+    
+}
+
+add();
+
+
+
+function addsion(a, b) {
+    console.log(50 + 50);
+    
+}
+
+addsion();
+
+
+function add1() {
+    
+    console.log( "rahul saini"); 
+
+    
+}
+add1();
+
+
+// function all type exmpal
+  
+
+ 
+
+
+
+
